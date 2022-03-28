40c213241321fa2096d9f2841f457a80b08c1470
Iann Esteban <ciannesteban@gmail.com>
2022-03-28 21:31:39 +0400

    updated sldier setting

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     const defTestimonialOption = {
         center: true,
         loop:true,
-        margin: 20,
+        margin: 50,
         //autoWidth: true,
         autoplay: false,
         nav: false,
@@ -13,16 +13,21 @@ $(document).ready(function(){
         responsive: {
            0:{
                stagePadding: 0,
-               items: 1.1,
-               margin: 0,
+               items: 1.065,
+               margin: 20,
                center: false,
                //loop: false,
                dotsEach: 1,
               // dots: true,
 
            },
-           990: {
-               stagePadding: 50,
+           768: {
+                stagePadding: 130,
+                items: 1,
+                margin: 30
+           },
+           992: {
+               stagePadding: 150,
                items: 1,
            },
            1200: {
@@ -41,17 +46,12 @@ $(document).ready(function(){
     }
 
     $('.testimonial-carousel').owlCarousel(defTestimonialOption)
-
-   // const dots = $( ".owl-dots" )
-        //dots.wrap( "<div class='container'></div>" )
-            
     const testiDot = $( ".testimonial-carousel .owl-dot" )
         testiDot.css("width", `calc(100% / ${ testiDot.length } )`)
 
     const defBlogOption = {
             loop:true,
-            margin: 10,
-            items: 3,
+            margin: 0,
             autoplay: false,
             nav: true,
             smartSpeed: 1000,
@@ -59,17 +59,18 @@ $(document).ready(function(){
             // autoWidth: true,
             stagePadding: 230,
             dots: false,
-            startPosition: 1,
             responsive: {
-                0: { items: 1.3, stagePadding: 50, margin: 0, dots: true, loop: false },
-                768:{ items: 2.3, stagePadding: 50, margin: 0, dots: true, loop: false },
-                1200: { items: 3 }
+                0: { items: 1.065, stagePadding: 0, margin: 20, dots: true, dotsEach: 1, loop: false, nav: false },
+                768:{ items: 2, stagePadding: 100, margin: 30, dots: true, startPosition: 1 },
+                1200: { items: 2, margin: 30, stagePadding: 100 },
+                1400: { items: 3, margin: 30, stagePadding: 100 },
+                1800:{ items: 3, margin: 50, startPosition: 320 }
             }
     }
     $('.blog-carousel').owlCarousel(defBlogOption)
 
-
-    const owl = $('.card-article-carousel');
+    const blogDot = $('.blog-carousel .owl-dot')
+        blogDot.css("width", `calc(100% / ${ blogDot.length } )`)
 
     const defCardOption = {
             dots: true,
@@ -78,12 +79,15 @@ $(document).ready(function(){
             margin: 30,
             responsive: {
                 0: {
-                    margin: 5,
-                    items: 1.1,
+                    margin: 0,
+                    items: 1.07,
+                    dots: true,
                     dotsEach: 1,
                 }
             }
         }
+
+    const owl = $('.card-article-carousel')
 
     if ( $(window).width() < 992 ) {
         owl.owlCarousel(defCardOption)
@@ -107,8 +111,8 @@ $(document).ready(function(){
             }
         } else {
             if ( !$('.owl-carousel').hasClass('off') ) {
-                owl.addClass('off').trigger('destroy.owl.carousel');
-                owl.find('.owl-stage-outer').children(':eq(0)').unwrap();
+                owl.addClass('off').trigger('destroy.owl.carousel')
+                owl.find('.owl-stage-outer').children(':eq(0)').unwrap()
             }
         }
 
